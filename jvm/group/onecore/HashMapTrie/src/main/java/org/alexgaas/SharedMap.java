c52f3b123a0c247aecd4da4fc804bfd392699aa2
Alex Gaas <alex.gaas@slalom.com>
2023-12-12 16:28:04 -0700

    Refactor HAMTv.

--- jvm/group/onecore/HashMapTrie/src/main/java/org/alexgaas/SharedMap.java
+++
@@ -1,9 +0,0 @@
-package org.alexgaas;
-
-import java.util.Map;
-
-public interface SharedMap<K,V> extends Map<K, V> {
-    SharedMap<K, V> with(K key, V value);
-
-    SharedMap<K,V> without(K key);
-}
